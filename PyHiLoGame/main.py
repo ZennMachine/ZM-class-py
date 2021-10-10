6e1a5f9d171c952f71402ead4f0260e59acb4238
ZENNMACHINE <elijahmc@protonmail.com>
2021-10-10 21:28:20 +0800

    Fixed the games bug which loses the game when youre right. Also added an introduction message and formatted the text for better clarity and readability.

--- PyHiLoGame/main.py
+++ PyHiLoGame/main.py
@@ -1,33 +1,40 @@
 import random
 
+A = 1
 J = 11
 Q = 12
 K = 13
 
-cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
+cards = [ A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K ]
 
 
 def get_new_card():
-    card = random.randrange(0, 12)
+    card = random.choice(cards)
     return card
 
 
-def compare_card(old_card, new_card):
+def new_card_is_higher(old_card, new_card):
     if old_card > new_card:
-        return True
-    else:
         return False
+    else:
+        return True
 
 
 def game_run():
+    print("Welcome to the ZM Higher or Lower Game! \n The game will deal you a card,\n" 
+        "each time you must guess whether the next card dealt is higher or lower than the previously dealt card.\n"
+        "Everytime you get the answer right, you earn a point.\n If you guess wrong however, the game ends.\n"
+        "--------------------------------------------")
+    # Game loop runs continuously until the answer given is wrong
     is_wrong = False
     score = 0
+
+    old_card = get_new_card()
     while not is_wrong:
-        old_card = get_new_card()
-        print(get_new_card(), "Is the next card higher or lower?")
+        print("Your card is: (",old_card , ") .\n Is the next card higher or lower?")
         user_in = input("h or l? ")
         new_card = get_new_card()
-        is_higher = compare_card(old_card, new_card)
+        is_higher = new_card_is_higher(old_card, new_card)
         if user_in == "h" and is_higher:
             print(new_card)
             print("Correct")
